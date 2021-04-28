da3c9079cb77f79cf6496ef56ebce91f03b6418e
Richard <74141398+iwhync@users.noreply.github.com>
2021-04-28 15:46:11 +0100

    Create grecomdiv.py

---
+++ grecomdiv.py
@@ -0,0 +1,17 @@
+import re
+x = input("Input: ")
+x = re.findall(r"[0-9]+",x)
+x = [int(x) for x in x]
+a = x.pop(0)
+end = []
+for _ in range(a+1):
+    one = x.pop(0)
+    two = x.pop(0)
+    if one > tw
+        p = math.gcd(one, two)
+        q = int((two * one) / p)
+        print(f"({p} {q})")  
+    elif two > one:
+        p = math.gcd(two, one)
+        q = int((one * two) / p)
+        print(f"({p} {q})")
